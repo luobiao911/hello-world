499d02d0b3a151d0825092bcccc4c19884480fd9
luobiao <luobiao@luobiaodeMacBook-Air.local>
2019-02-25 21:45:11 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.youmeek.demo;
+
+public class HelloWorld {
+
+    int number;
+    //number = 100;
+
+    public static void main(String [] args){
+
+        System.out.println("HelloWorld");
+        System.out.println(2.0-1.1);
+        double x = 4;
+        double y = Math.sqrt(x);
+        System.out.println(y);
+        System.out.println(Math.pow(20,2.5));
+        System.out.println(Math.floorMod(7,2));
+
+
+    }
+}
